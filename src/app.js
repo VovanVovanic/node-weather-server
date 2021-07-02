afb5853d285216231c1355591789751a5cd2034c
Vladimir <vladimirplotnikov371@gmail.com>
2021-07-02 14:02:32 +0300

    heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 const app = express()
 
-
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "../public")));
 const viewsPath = path.join(__dirname, "../templates/views")
@@ -93,6 +93,6 @@ app.get('*', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up');
 })
